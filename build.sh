798b224365e72cbcc179ff4ff6b4f4b0001092b8
Terrance A. Snyder <terrance.a.snyder@gmail.com>
2023-08-31 01:47:46 +0000

    Test.

---
+++ build.sh
@@ -0,0 +1,17 @@
+npm cache clear --force
+
+npm config set fetch-retry-mintimeout 20000
+
+npm config set fetch-retry-maxtimeout 120000
+
+# sudo npm install -g lerna@5.6.2
+
+npm install
+
+# npm run refresh
+# npm run clean
+# npm run build:clean
+# npm run build
+
+npm run initialize
+npm run build
